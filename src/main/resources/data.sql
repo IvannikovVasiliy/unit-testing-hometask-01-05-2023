476c2bf31a04185c2e3dc2def5dbe55343e08165
IvannikovVasiliy <ivannikovvasiliya2001@gmail.com>
2023-05-04 12:46:11 +0300

    do liquibase

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into person
-values (1, 'Jack Daniels', 'jackdaniels@example.com'),
-       (2, 'George Dickel', 'georgedickel@example.com'),
-       (3, 'George Dickelson', 'georgedickelson@example.com');
+-- insert into person
+-- values (1, 'Jack Daniels', 'jackdaniels@example.com'),
+--        (2, 'George Dickel', 'georgedickel@example.com'),
+--        (3, 'George Dickelson', 'georgedickelson@example.com');
